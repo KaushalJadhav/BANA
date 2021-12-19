fb7967b28dbfc2f4080fda4f533a320d2f05302b
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2021-12-19 20:55:29 +0530

    update

--- utils/losses.py
+++ utils/losses.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch.nn.functional as F
 import torch.nn as nn
 
-def get_loss_ce(self, y_pred, ycrf,num_classes):
+def get_loss_ce(y_pred, ycrf,num_classes):
     '''
     y_pred and y_crf are batches
     '''
@@ -16,3 +16,25 @@ def get_loss_ce(self, y_pred, ycrf,num_classes):
     num=torch.sum(torch.log(y_pred[s_class]))
     return -num/denom 
 
+def get_loss_wce(y_pred,ycrf,yret,feature_map,classifier_weight,num_classes):
+
+    cos=torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+    n_classes_arr=torch.from_numpy(np.arange(num_classes))
+    # Correlation Map
+    correlation_map = torch.zeros((num_classes+1, feature_map.shape[1], feature_map.shape[2])).cuda()
+    correlation_map = 1 + cos(feature_map[None,...], classifier_weight)   # 3d 
+
+    # Confidence Map
+    correlation_map_cstar = torch.zeros((feature_map.shape[1], feature_map.shape[2])).cuda()
+    idx = (ycrf[:,:,None] ==n_classes_arr)
+    idx= torch.permute(idx,(2,0,1))
+    correlation_map_cstar[idx[i] for i in range num_classes] = correlation_map[idx] # check
+    confidence_map = (correlation_map_cstar / torch.max(correlation_map, dim=0).values ) ** self.gamma  # 2d
+        
+    # Final Loss
+    denom = 0.0
+    numer = 0.0
+    not_s_class = torch.logical_not(( ycrf[:,:,None] ==n_classes_arr) & (yret[:,:,None] ==n_classes_arr))
+    denom = torch.sum( confidence_map[not_s_class] )
+    numer = torch.sum( confidence_map[not_s_class] * torch.log(y_pred[not_s_class]) )
+    return -numer/denom
--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -2,18 +2,18 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class PolynomialLR(_LRScheduler):
-    def __init__(self, optimizer, step_size, iter_max, power, last_epoch=-1):
-        super(PolynomialLR, self).__init__(optimizer, last_epoch)
-        self.optimizer=optimizer 
+    def __init__(self, optimizer,iter_max=-1,last_epoch=-1):
+        super(PolynomialLR, self).__init__(optimizer, last_epoch) 
+        self.iter_max=iter_max
 
-    def polynomial_decay(self, lr,last_epoch,iter_max,power):
+    def polynomial_decay(self, lr,power):
         return lr * (1 - float(self.last_epoch) / self.iter_max) ** power
 
-    def __call__(self,last_epoch,step_size,iter_max,power):
+    def __call__(self,step_size,power):
         if (
-            (last_epoch == 0)
-            or (last_epoch % step_size != 0)
-            or (last_epoch > iter_max)
+            (self.last_epoch == 0)
+            or (self.last_epoch % step_size != 0)
+            or (self.last_epoch > self.iter_max)
         ):
             return [group["lr"] for group in self.optimizer.param_groups]
-        return [self.polynomial_decay(lr,last_epoch,iter_max,power) for lr in self.base_lrs]
+        return [self.polynomial_decay(lr,self.iter_max,power) for lr in self.base_lrs]
