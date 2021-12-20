c4f51e9a823eddaea8b13fb95f724beddcbe9d88
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2021-12-20 17:34:54 +0530

    wce loss batch is to be added

--- utils/losses.py
+++ utils/losses.py
@@ -8,33 +8,31 @@ def get_loss_ce(y_pred, ycrf,num_classes):
     '''
     y_pred and y_crf are batches
     '''
-    denom = 0.0
-    numer = 0.0
     n_classes_arr=torch.from_numpy(np.arange(num_classes+1))
     s_class = ( ycrf[:,:,:,None] ==n_classes_arr) & ( yret[:,:,:,None] ==n_classes_arr )
     denom=torch.sum(s_class)
     num=torch.sum(torch.log(y_pred[s_class]))
     return -num/denom 
 
-def get_loss_wce(y_pred,ycrf,yret,feature_map,classifier_weight,num_classes):
+def get_loss_wce(y_pred,ycrf,yret,feature_map,classifier_weight,num_classes,batchsize):
 
     cos=torch.nn.CosineSimilarity(dim=1, eps=1e-6)
     n_classes_arr=torch.from_numpy(np.arange(num_classes))
-    # Correlation Map
-    correlation_map = torch.zeros((num_classes+1, feature_map.shape[1], feature_map.shape[2])).cuda()
-    correlation_map = 1 + cos(feature_map[None,...], classifier_weight)   # 3d 
+    # Correlation Map (CHECK BATCH)
+    correlation_map = torch.zeros((num_classes+1,feature_map.shape[1], feature_map.shape[2])).cuda()  # No change
+    correlation_map[:num_classes] = 1 + cos(feature_map[None,...], classifier_weight[:num_classes])   # 3d 
 
     # Confidence Map
     correlation_map_cstar = torch.zeros((feature_map.shape[1], feature_map.shape[2])).cuda()
-    idx = (ycrf[:,:,None] ==n_classes_arr)
+    idx = (ycrf[:,:,None] == n_classes_arr)
     idx= torch.permute(idx,(2,0,1))
-    correlation_map_cstar[idx[i] for i in range num_classes] = correlation_map[idx] # check
-    confidence_map = (correlation_map_cstar / torch.max(correlation_map, dim=0).values ) ** self.gamma  # 2d
+    correlation_map_cstar[idx[i] for i in range(num_classes)] = correlation_map[idx] # check
+    confidence_map = (correlation_map_cstar / torch.max(correlation_map, dim=0).values) ** self.gamma  # 2d
         
     # Final Loss
-    denom = 0.0
-    numer = 0.0
     not_s_class = torch.logical_not(( ycrf[:,:,None] ==n_classes_arr) & (yret[:,:,None] ==n_classes_arr))
-    denom = torch.sum( confidence_map[not_s_class] )
-    numer = torch.sum( confidence_map[not_s_class] * torch.log(y_pred[not_s_class]) )
+    not_s_class = torch.permute(not_s_class,(2,0,1))
+    t = confidence_map[not_s_class[i] for i in range(num_classes) ]
+    denom = torch.sum(t)
+    numer = torch.sum( t * torch.log(y_pred[not_s_class]))
     return -numer/denom
