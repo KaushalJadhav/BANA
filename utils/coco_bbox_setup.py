d81e3ead14f9e76bfb5c838f907a3d80ba4f902a
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2021-12-24 11:20:53 +0530

    optimized coco_bbox_setup.py

--- utils/coco_bbox_setup.py
+++ utils/coco_bbox_setup.py
@@ -6,10 +6,11 @@ try:
     import imagesize
 except ModuleNotFoundError:
     pass
+import time
 
 root_dir = os.getcwd()
-directory = os.path.join(root_dir,"val2017")
-ann_path = os.path.join(root_dir,'annotations/instances_val2017.json')
+directory = os.path.join(root_dir,"train2017")  # need to update this 
+ann_path = os.path.join(root_dir,'annotations/instances_train2017.json')
 annotations = COCO(ann_path)
 cat_ids = annotations.getCatIds()
 img_ids = []
@@ -17,39 +18,36 @@ for cat in cat_ids:
     img_ids.extend(annotations.getImgIds(catIds=cat))      
 img_ids = list(set(img_ids))
 
+output_dir = os.path.join(root_dir,"BgMaskfromBoxes")
+if not os.path.exists(output_dir):
+  os.mkdir(output_dir)
+
 j=0        
 for img_id in img_ids:
         
     ann_ids = annotations.getAnnIds(imgIds=img_id)
     coco_annotation = annotations.loadAnns(ann_ids)
-    # number of objects in the image
-    num_objs = len(coco_annotation)
 
-    # Bounding boxes for objects
-    # In coco format, bbox = [xmin, ymin, width, height]
-    # In pytorch, the input should be [xmin, ymin, xmax, ymax]
-    bboxes = []
-    for i in range(num_objs):
-        xmin = coco_annotation[i]['bbox'][0]
-        ymin = coco_annotation[i]['bbox'][1]
-        xmax = xmin + coco_annotation[i]['bbox'][2]
-        ymax = ymin + coco_annotation[i]['bbox'][3]
-        bboxes.append([int(xmin), int(ymin), int(xmax), int(ymax)])
-       
+    # number of objects in the image
+    num_objs = len(coco_annotation) 
     path = annotations.loadImgs(img_id)[0]['file_name']
     img_path =os.path.join(directory,path)
-    # img = np.array(Image.open(img_path))
-    #h,w,dim = img.shape
-    img_size =imagesize.get(img_path)  # This will make it a bit faster...Check whether to keep
+    img_size =imagesize.get(img_path)
     mask = np.ones((img_size[0],img_size[1]),dtype=np.uint8)
-    for bbox in bboxes:
-        (xmin,ymin,xmax,ymax) = bbox
-        mask[ymin:ymax,xmin:xmax] = 0
+    
+    # Bounding boxes for objects
+    # In coco format, bbox = [xmin, ymin, width, height]
+    # In numpy, the input should be [xmin, ymin, xmax, ymax]
+
+    for i in range(num_objs):
+      temp=coco_annotation[i]['bbox']
+      xmin = int(temp[0]) 
+      ymin = int(temp[1])
+      xmax = xmin + int(temp[2])
+      ymax = ymin + int(temp[3])
+      mask[ymin:ymax,xmin:xmax] = 0
     
     im = Image.fromarray(mask)
     output_name = path[:-4] + ".png"
-    output_dir = os.path.join(root_dir,"BgMaskfromBoxes")
     output_path = os.path.join(output_dir,output_name)
-    j=j+1
-    #print(output_name, j)
     im.save(output_path)
