11fbb46e2063f533580470a52a14e7718a0fbe3d
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2022-01-11 16:15:59 +0530

    Updated NAL

--- utils/losses.py
+++ utils/losses.py
@@ -25,33 +25,40 @@ def get_loss_ce(y_pred, ycrf,yret,num_classes):
 
 def get_loss_wce(y_pred,ycrf,yret,feature_map,classifier_weight,num_classes,gamma):
      
-    # cos=torch.nn.CosineSimilarity(dim=2, eps=1e-6)  # 1024
+    cos=torch.nn.CosineSimilarity(dim=2, eps=1e-6)  # 1024
     n_classes_arr=torch.from_numpy(np.arange(num_classes)).to('cuda')
 
     feature_map=F.interpolate(feature_map,size=(321,321))
-    classifier_weight=F.interpolate(classifier_weight,size=(321,321))
-    correlation_map = torch.zeros((feature_map.shape[0],num_classes,feature_map.shape[2], feature_map.shape[3])).cuda() 
+   
+    correlation_map = torch.zeros((feature_map.shape[0],num_classes,321,321)).cuda() 
     # Confidence Map
-    correlation_map_cstar = torch.zeros((feature_map.shape[0],feature_map.shape[2], feature_map.shape[3])).cuda()
+    correlation_map_cstar = torch.zeros((feature_map.shape[0],321,321)).cuda()
 
     classifier_weight= classifier_weight.unsqueeze(0)  # shape  = (1,num_classes,1024,...,...)
     feature_map= feature_map.unsqueeze(1)              # shape = (batchsize,1,1024,...,...)
-    correlation_map[:,:num_classes,...] = 1 + cosinesimilarity(feature_map,classifier_weight,dim=2,eps=1e-6)
-    # correlation_map[:,:num_classes,...] = 1 + cos(feature_map,classifier_weight)  # shape- batchsize,num_classes,...,...
-  
+    
+    correlation_map[:,:num_classes,...] = 1 + cos(feature_map,classifier_weight)  # shape- batchsize,num_classes,...,...
+
     idx = (ycrf[:,:,:,None] == n_classes_arr)    # will be of shape batchsize,...,...,num_classes
     idx= torch.permute(idx,(0,3,1,2))  # we want batchsize, num_classes,...,...
-    print('\ni ',idx.shape)
-    print('\ncmcs ',correlation_map_cstar.shape)
-    print('\ncm ',correlation_map.shape)
+  
     for i in range(num_classes):
-      correlation_map_cstar[idx[:,i,:,:]] = correlation_map[idx] 
+      t=idx[:,i,:,:]
+      correlation_map_cstar[t] = correlation_map[:,i,:,:][t] 
     confidence_map = (correlation_map_cstar / torch.max(correlation_map, dim=1).values) ** gamma 
         
     # Final Loss
+    denom=0
+    numer=0
     not_s_class = torch.logical_not(( ycrf[:,:,:,None] ==n_classes_arr) & (yret[:,:,:,None] ==n_classes_arr))  # size- batchsize,...,...,num_classes
-    not_s_class = torch.permute(not_s_class,(0,3,1,2))  # batchsize,num_classes,...,... 
-    t = confidence_map[not_s_class[:,n_classes_arr,:,:]]
-    denom = torch.sum(t)
-    numer = torch.sum( t * torch.log(y_pred[not_s_class]))
+    not_s_class = torch.permute(not_s_class,(0,3,1,2))  # batchsize,num_classes,...,...
+
+    # No need of for loop if the following line works
+    # t = confidence_map[:,None,:,:].expand(-1,21,-1,-1)[not_s_class[:,n_classes_arr,:,:]]
+    for i in range(num_classes):
+      t= not_s_class[:,i,:,:]
+      denom += torch.sum( confidence_map[t] )
+      numer += torch.sum( confidence_map[t] * torch.log(y_pred[:,i,:,:][t]))
+    # denom = torch.sum(t)
+    # numer = torch.sum( t * torch.log(y_pred[not_s_class]))
     return -numer/denom
