7bfff9fd77a9fcfefde17e971a88d86668ed0e77
Kaushal Jadhav <82267462+KaushalJadhav@users.noreply.github.com>
2022-01-26 16:41:42 +0530

    minor change

--- utils/voc_bbox_setup.py
+++ utils/voc_bbox_setup.py
@@ -4,10 +4,13 @@ from PIL import Image
 import xml.etree.cElementTree as et
 
 root_dir = os.getcwd() + '/VOCdevkit/VOC2012/'
-directory = os.path.join(root_dir,"Annotations")
+annot_dir = os.path.join(root_dir,"Annotations")
+output_dir = os.path.join(root_dir,"BgMaskfromBoxes")
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
 
-for filename in os.listdir(directory):
-    file_annot = os.path.join(directory,filename)
+for filename in os.listdir(annot_dir):
+    file_annot = os.path.join(annot_dir,filename)
     tree=et.parse(file_annot)
     root=tree.getroot()
     bounding_boxes = []
@@ -19,9 +22,9 @@ for filename in os.listdir(directory):
         for height in root.iter('height'):
             h = int(height.text)
             img_size.append(h)
+    #flipped dimensions here
+    mask = np.ones((img_size[1],img_size[0]),dtype=np.uint8)
     for bndbox in root.iter('bndbox'):
-        bbox = [0]*4
-        
         for xmin in bndbox.iter('xmin'):
             x_min = int(float(xmin.text))
         for xmax in bndbox.iter('xmax'):
@@ -30,23 +33,10 @@ for filename in os.listdir(directory):
             y_min = int(float(ymin.text))
         for ymax in bndbox.iter('ymax'):
             y_max = int(float(ymax.text))
-        bbox[0] = x_min
-        bbox[1] = y_min
-        bbox[2] = x_max
-        bbox[3] = y_max
-        bbox = tuple(bbox)
-        bounding_boxes.append(bbox)
-    #flipped dimensions here
-    mask = np.ones((img_size[1],img_size[0]),dtype=np.uint8)
-    for bbox in bounding_boxes:
-        (xmin,ymin,xmax,ymax) = bbox
         mask[ymin:ymax,xmin:xmax] = 0
-    
+        
     im = Image.fromarray(mask)
     output_name = filename[:-4] + ".png"
-    output_dir = os.path.join(root_dir,"BgMaskfromBoxes")
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
     output_path = os.path.join(output_dir,output_name)
     print(output_name)
     im.save(output_path)
